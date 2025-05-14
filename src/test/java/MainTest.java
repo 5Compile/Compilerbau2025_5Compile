2251574f54ca6bda2168cb5d12a3040c3ef11060
Tobias Schuhmacher <i23036@hb.dhbw-stuttgart.de>
2025-05-14 10:58:23 +0200

    Projekt initiiert

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MainTest {
+
+    @Test
+    void testHello() {
+        assertEquals(2, 1 + 1);
+    }
+}
