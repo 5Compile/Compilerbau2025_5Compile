b56c4509410d01f9012279fb0dd8bfed20969ded
i23007 <i23007@hb.dhbw-stuttgart.de>
2025-05-28 03:02:17 +0200

    first try on typed Programm and Classes (for empty class)

--- src/main/java/TypedAST/TypedClasses/TypedFieldDecl.java
+++ src/main/java/TypedAST/TypedClasses/TypedFieldDecl.java
@@ -1,4 +1,14 @@
 package TypedAST.TypedClasses;
 
+import AST.FieldDecl;
+import TypedAST.Type;
+
 public class TypedFieldDecl {
+    public String typedecl;
+    public String name;
+    public Type type;
+
+    TypedFieldDecl(FieldDecl untypedFieldDecl){
+
+    }
 }
--- src/main/java/TypedAST/TypedClasses/TypedProgram.java
+++ src/main/java/TypedAST/TypedClasses/TypedProgram.java
@@ -1,4 +1,18 @@
 package TypedAST.TypedClasses;
 
+import AST.ClassDecl;
+import AST.Program;
+
+import java.util.List;
+
 public class TypedProgram {
+    List<TypedClassDecl> classes;
+
+    TypedProgram(Program untypedProgram){
+        for (ClassDecl classDecl: untypedProgram.classes())
+        {
+            this.classes.add(new TypedClassDecl(classDecl));
+        }
+
+    }
 }
