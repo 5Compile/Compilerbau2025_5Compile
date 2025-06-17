767a093b34941563150ccecafafd150faede57e9
john04022 <i23018@hb.dhbw-stuttgart.de>
2025-06-17 08:11:11 +0200

    Methode gibt ganzes Program zurück nicht nur classDecl

--- src/test/java/ASTTests/Feature/AST_ClassDecl.java
+++ src/test/java/ASTTests/Feature/AST_ClassDecl.java
@@ -1,17 +1,25 @@
 package ASTTests.Feature;
 
-import AST.ClassDecl;
+import AST.*;
 
 import java.util.List;
 import java.util.Optional;
 
 public class AST_ClassDecl {
-    public static ClassDecl get() {
-        return new ClassDecl(
-                "ClassDecl",          // Klassenname
-                List.of(),            // leere Felder
-                List.of(),            // leere Methoden
-                Optional.empty()
+
+    /**
+     *  public class ClassDecl { }
+     */
+    public static Program get() {
+        // leere Klasse
+        ClassDecl clazz = new ClassDecl(
+                "ClassDecl",
+                List.of(),            // keine Felder
+                List.of(),            // keine Methoden
+                Optional.empty()      // keine main-Methode
         );
+
+        // vollständiges Programm (genau eine Klasse)
+        return new Program(List.of(clazz));
     }
 }
