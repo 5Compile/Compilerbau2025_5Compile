a741b6bc27d3ddf21536e50f15e1dc93dd439be5
i23007 <i23007@hb.dhbw-stuttgart.de>
2025-06-26 00:05:50 +0200

    small fix addition

--- src/main/java/AST/MethodDecl.java
+++ src/main/java/AST/MethodDecl.java
@@ -1,12 +1,15 @@
 package AST;
 
+import TypedAST.TypedClasses.TypedLocalVarDecl;
 import TypedAST.TypedClasses.Visitor;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public record MethodDecl(String name,
                          String returnType,
                          List<Parameter> parameters,
+                         List<LocalVarDecl> localVariables,
                          Block body) implements MiniJava {
     @Override
     public <T> T accept(Visitor<T> visitor) {
