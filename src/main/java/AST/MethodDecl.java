cd87895e6842eeba3bebcc5b40e0fb06a9199c13
i23007 <i23007@hb.dhbw-stuttgart.de>
2025-06-26 00:32:12 +0200

    small fix revert

--- src/main/java/AST/MethodDecl.java
+++ src/main/java/AST/MethodDecl.java
@@ -9,7 +9,6 @@ import java.util.List;
 public record MethodDecl(String name,
                          String returnType,
                          List<Parameter> parameters,
-                         List<LocalVarDecl> localVariables,
                          Block body) implements MiniJava {
     @Override
     public <T> T accept(Visitor<T> visitor) {
--- src/main/java/TypedAST/TypedClasses/TypedMethodDecl.java
+++ src/main/java/TypedAST/TypedClasses/TypedMethodDecl.java
@@ -10,14 +10,12 @@ import java.util.List;
 public class TypedMethodDecl implements TypedMiniJava {
     String name;
     List<TypedParameter> parameters;
-    List<TypedLocalVarDecl> localVars;
     TypedBlock body;
     Type type;
 
-    TypedMethodDecl(String name, List<TypedParameter> typedParameterList, List<TypedLocalVarDecl> localVars, TypedBlock typedBody, Type type){
+    TypedMethodDecl(String name, List<TypedParameter> typedParameterList, TypedBlock typedBody, Type type){
         this.name = name;
         this.parameters = typedParameterList;
-        this.localVars = localVars;
         this.body = typedBody;
         this.type = type;
     }
