34e2f85a579688090eccdb5a4afa91c3bd19cad6
jonat <i23018@dhbw-stuttgart.de>
2025-06-25 23:55:09 +0200

    End2End Files und End2End Tests hinzugefügt für ReflectionUtility

---
+++ src/test/java/End2EndTests/Feature/End2End_StringProcessor.java
@@ -0,0 +1,29 @@
+package End2EndTests.Feature;
+
+import End2EndTests.ReflectionUtility;
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class End2End_StringProcessor {
+    private ReflectionUtility util;
+
+    @BeforeEach
+    void init() throws Exception {
+        util = new ReflectionUtility("src/test/testing_files/StringProcessor.java", "StringProcessor");
+    }
+
+    @Test
+    void testStringMethods() throws Exception {
+        assertEquals("HELLO", util.invokeMethod("toUpper", new Class[]{String.class}, "hello"));
+        assertEquals("olleh", util.invokeMethod("reverse", new Class[]{String.class}, "hello"));
+        assertEquals(5, util.invokeMethod("length", new Class[]{String.class}, "hello"));
+    }
+
+    @Test
+    void testMethodInfo() throws Exception {
+        assertEquals(3, util.getMethodCount());
+        assertTrue(util.getMethodNames().contains("toUpper"));
+        assertEquals("int", util.getMethodReturnType("length", String.class));
+    }
+}
---
+++ src/test/testing_files/End2End_MathUtils.java
@@ -0,0 +1,14 @@
+public class MathUtils {
+
+    public int square(int number) {
+        return number * number;
+    }
+
+    public int cube(int number) {
+        return number * number * number;
+    }
+
+    public boolean isEven(int number) {
+        return number % 2 == 0;
+    }
+}
